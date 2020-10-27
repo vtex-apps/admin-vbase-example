9076d0012707dda572980d8c7c13c35581721a97
Julia Rocha <juliaafr@outlook.com>
2020-10-27 17:43:42 -0300

    removed node modules

--- node_modules/eslint-config-vtex/rules/prettier.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/prettier/eslint-plugin-prettier
-module.exports = {
-  extends: ['plugin:prettier/recommended'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-  },
-}
